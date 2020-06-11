885d049c2408a47023dd67677ab759ab10a42ff9
Yara Elmalah <yaraelmalah2016@gmail.com>
2020-06-11 08:55:08 +0200

    Edit Member Page

--- Admin/layout/js/custom-admin.js
+++ Admin/layout/js/custom-admin.js
@@ -1,9 +1,23 @@
 $(function(){
 	'use strict';
+	//Remove Placeholder on Focus
 	$('[placeholder]').focus(function(){
 		$(this).attr('data-text', $(this).attr('placeholder'));
 		$(this).attr('placeholder', '');
 	}).blur(function(){
 		$(this).attr('placeholder', $(this).attr('data-text'));
+	})
+	//Add Asterisk on Required Files
+	$('input').each(function(){
+		if($(this).attr('required') === 'required'){
+			$(this).after('<span class="asterisk">*</span>');
+		}
+	})
+	//Convert Password Field To Text Filed
+	var passField = $('.password');
+	$('.fa-low-vision').hover(function(){
+		passField.attr('type', 'text');
+	}, function(){
+		passField.attr('type', 'password');
 	})
 })
--- Admin/members.php
+++ Admin/members.php
@@ -13,14 +13,50 @@ if(isset($_SESSION['username'])){
 $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage'; 
 //Assign do;
 //Create Our Page Depends on the $_GET
-if($do == 'Manage'){
-	echo "Welcome To Manage Page";
-} elseif($do == 'Add'){
-	echo "Welcome to the Add Page";
-} elseif ($do == "Edit") {?>
-	<h1 class="text-center">Edit Profile</h1>
+if($do == 'Manage'){ //Manage Page?>
+	<h1 class="text-center">Manage Members</h1>
 	<div class="container">
-		<form class="form-horizontal form-lg">
+	<a href='members.php?do=Add' class="btn btn-primary">Add New Member</a>
+	<div class="table-responsive">
+		<table class="table table-bordered text-center main-table">
+			<tr>
+				<th class="text-uppercase">#id</th>
+				<th class="text-capitalize">username</th>
+				<th class="text-capitalize">email</th>
+				<th class="text-capitalize">full name</th>
+				<th class="text-capitalize">register date</th>
+				<th class="text-capitalize">control</th>
+			</tr>
+				<?php 
+			//We can add to the querry (WHERE groupID != 1 as no the the Admin included)
+					$stmt = $connect->prepare("
+						SELECT * FROM `shop-users`  
+						");
+					$stmt-> execute(); //Execute the Statement
+					//Assign To a Variable
+					$rows = $stmt-> FetchAll(); //get All members
+					foreach ($rows as $row) {
+						 echo "<tr>";
+						 echo "<td>" . $row['UserID'] . "</td>";
+						 echo "<td>" . $row['username'] . "</td>";
+						 echo "<td>" . $row['Email'] . "</td>";
+						 echo "<td>" . $row['Full-Name'] . "</td>";
+						 echo "<td>" . " " . "</td>";
+						 echo "<td>" . "<a href='members.php?do=Edit&userid=" . 
+						 $row['UserID'] .  "' class='btn btn-success'>Edit</a>" . 
+					"<a href='#' class='btn btn-danger'>Delete</a>"; 
+						 echo "</tr>";
+					}
+			?>
+			
+		</table>
+	</div>
+     </div>
+<?php } elseif($do == 'Add'){ //Add Member Page?>
+
+	<h1 class="text-center">Add New Member</h1>
+	<div class="container">
+		<form class="form-horizontal form-lg" action="?do=Insert" method="POST">
 			<div class="form-group">
 				<!--Start Username-->
 				<div class="form-group">
@@ -28,7 +64,7 @@ if($do == 'Manage'){
 					Username
 				</label>
 					<div class="col-sm-10 col-md-6">
-						<input type="text" name="username" class="form-control" autocomplete="off">
+						<input type="text" name="username" class="form-control" autocomplete="off" required="required" placeholder="The user will login with">
 					</div>
 				</div>
 				<!--End Username-->
@@ -37,9 +73,10 @@ if($do == 'Manage'){
 					<label class="col-sm-2 control-label">
 					Password
 				</label>
-					<div class="col-sm-10 col-md-6">
-						<input type="password" name="password" class="form-control" autocomplete="new-password">
-					</div>
+				<div class="col-sm-10 col-md-6">
+				<input type="password" name="password" class="form-control password" placeholder="Enter Strong Password"
+				autocomplete="new-password" required="required" > <i class="fas fa-low-vision"></i>
+				</div>
 				</div>
 				<!--End Password-->
 				<!--Start Email-->
@@ -48,7 +85,8 @@ if($do == 'Manage'){
 					Email
 				</label>
 					<div class="col-sm-10 col-md-6">
-						<input type="email" name="email" class="form-control" autocomplete="off">
+						<input type="email" name="email" class="form-control" autocomplete="off" 
+						required="required" placeholder="Enter a Valid Email">
 					</div>
 				</div>
 				<!--End Email-->
@@ -58,7 +96,87 @@ if($do == 'Manage'){
 					Full-Name
 				</label>
 					<div class="col-sm-10 col-md-6">
-						<input type="text" name="full-name" class="form-control" autocomplete="off">
+						<input type="text" name="full-name" class="form-control" autocomplete="off" required="required" placeholder="Your Full Name that Appear in Your Profile">
+					</div>
+				</div>
+				<!--End FullName-->
+				<!--Start Submit-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label"> 
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="submit" value="Add" class="btn btn-primary btn-lg">
+					</div>
+				</div>
+				<!--End FullName-->
+				
+			</div>
+		</form>
+	</div>
+
+<?php } elseif ($do == "Edit") { //Edit Page
+	//Edit from the id with short if condition
+	$userid = isset($_GET['userid']) 
+		&& is_numeric($_GET['userid'])? 
+		intval($_GET['userid']): 0; //That is our user that we would deal with
+		//Now We get the Record from database
+		$stmt = $connect->prepare("SELECT * from `shop-users`
+			WHERE UserID = ? 
+			LIMIT 1"); //get this user
+		$stmt->execute(array($userid));
+		$row = $stmt->fetch();
+		$count = $stmt-> rowCount();
+		//if We have a record then it must be > 0
+		if($count > 0 ){ ?>
+			<h1 class="text-center">Edit Profile</h1>
+	<div class="container">
+		<form class="form-horizontal form-lg" action="?do=Update" method="POST">
+			<div class="form-group">
+				<input type="hidden" name="userid" value="<?php echo $userid; ?>">
+				<!--Start Username-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Username
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="text" name="username" class="form-control" autocomplete="off" required="required" 
+						value="<?php echo $row['username']; ?>">
+					</div>
+				</div>
+				<!--End Username-->
+				<!--Start Password-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Password
+				</label>
+					<input type="hidden" value="<?php 
+					echo $row['Password']; ?>" name="old-pass">
+					<div class="col-sm-10 col-md-6">
+						<input type="password" name="new-pass" class="form-control" placeholder="Leave it blank if you don't want to change Your Password" 
+						autocomplete="new-password">
+					</div>
+				</div>
+				<!--End Password-->
+				<!--Start Email-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Email
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="email" name="email" class="form-control" autocomplete="off" 
+						required="required"
+						value="<?php echo $row['Email']; ?>">
+					</div>
+				</div>
+				<!--End Email-->
+				<!--Start FullName-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Full-Name
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="text" name="full-name" class="form-control" autocomplete="off" required="required"
+						value="<?php echo $row['Full-Name']; ?>">
 					</div>
 				</div>
 				<!--End FullName-->
@@ -74,18 +192,123 @@ if($do == 'Manage'){
 				
 			</div>
 		</form>
-	</div>
-<?php } 
+	</div> <?php
+		} else{
+			echo "Wrong There is no such user";
+		}
+	
+        } 
 elseif ($do == "Update") {
-	echo "Update";
+	if($_SERVER['REQUEST_METHOD'] === "POST"){
+		echo "<h1 class='text-center'>Update </h1>"; 
+		//Get Variables From the Form
+		$id = $_POST['userid'];
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$full = $_POST['full-name'];
+		//Password Trick
+		$pass = empty($_POST['new-pass'])? $_POST['old-pass'] :sha1($_POST['new-pass']);
+		//Validate The Form
+		$formErrors = [];
+
+		if(empty($username)){
+			$formErrors[] = "Username can't <strong>empty</strong>";
+		} 
+		if (empty($email)) {
+			$formErrors[] = "Username can't <strong>empty</strong>";
+		}
+		if (empty($full)) {
+			$formErrors[] = "Full Name can't <strong>empty</strong>";
+		} 
+		if (strlen($username) < 4) {
+			$formErrors[] = "Full Name can't be smaller than<strong> 4 Characters</strong>";
+		}
+		if (strlen($username) > 14) {
+			$formErrors[] = "Full Name can't be larger than<strong> 14 Characters</strong>";
+		}
+	     	
+			//Database Query
+			//Check if There is no errors 
+			if(empty($formErrors)){
+		    //Update Database with this info
+		     $stmt = $connect->prepare("UPDATE `shop-users` 
+			SET username = ? , Email = ? ,  `Full-Name` = ? where UserID = ?");
+	     	$stmt->execute(array($username, $email, $full, $id));
+	     	//Echo Success Message
+	     	echo "<div class='alert alert-success'>" .  $stmt->rowCount() . " Record Updated </div>";
+			} else{
+				//Get The Errors
+	          foreach ($formErrors as $error) {
+				echo "<div class='alert alert-danger'>"  . 
+				$error . "</div>";
+			}
+			}
+			
+		
+	
+	} else{
+		header('location: members.php');
+	}
 } 
 
-elseif ($do == "Add") {
-	echo "Add";
-} 
-elseif ($do == "Insert") {
-	echo "Insert";
-} 
+elseif ($do == "Insert") { //Insert Page
+       if($_SERVER['REQUEST_METHOD'] === "POST"){
+		echo "<h1 class='text-center'>Insert </h1>"; 
+		//Get Variables From the Form
+		$username = $_POST['username'];
+		$email = $_POST['email'];
+		$full = $_POST['full-name'];
+		$pass = $_POST['password'];
+		$hashedPass = sha1($pass);
+		//Validate The Form
+		$formErrors = [];
+
+		if(empty($username)){
+			$formErrors[] = "Username can't <strong>empty</strong>";
+		} 
+		if (empty($email)) {
+			$formErrors[] = "Username can't <strong>empty</strong>";
+		}
+		if (empty($full)) {
+			$formErrors[] = "Full Name can't <strong>empty</strong>";
+		} 	
+		if (empty($pass)) {
+			$formErrors[] = "Password can't <strong>empty</strong>";
+		} 
+		if (strlen($username) < 4) {
+			$formErrors[] = "Full Name can't be smaller than<strong> 4 Characters</strong>";
+		}
+		if (strlen($full) > 14) {
+			$formErrors[] = "Full Name can't be larger than<strong> 14 Characters</strong>";
+		} 
+		//Database Query
+			//Check if There is no errors 
+			if(empty($formErrors)){
+		    //Insert this info into Database 
+		   	$stmt =  $connect->prepare("INSERT INTO `shop-users`(username, Password, Email, `Full-Name`) 
+		   		VALUES (:user, :pass, :mail, :full)");
+		   	$stmt->execute(array(
+		   		':user' => $username,
+		   		':pass' => $hashedPass,
+		   		':mail' => $email,
+		   		':full' => $full
+		   	));
+	     	//Echo Success Message
+	     	echo "<div class='alert alert-success'>" .  $stmt->rowCount() . " Member Added</div>";
+			} else{
+				//Get The Errors
+	          foreach ($formErrors as $error) {
+				echo "<div class='alert alert-danger'>"  . 
+				$error . "</div>";
+			}
+			}
+			
+		
+
+      } else{
+		header('location: members.php');
+	}
+}
 elseif ($do == "Delete") {
 	echo "Delete";
 } 
