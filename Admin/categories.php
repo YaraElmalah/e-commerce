2b4eafd7103f021b40a7bfc7ac33ef0381269827
Yara Elmalah <yaraelmalah2016@gmail.com>
2020-06-19 18:10:17 +0200

    Edit Catgeory Page

--- Admin/categories.php
+++ Admin/categories.php
@@ -5,6 +5,10 @@
 ob_start(); //Output Buffering Start
 session_start();
 $pageTitle = 'Categories';
+$orderBy = 'Ordering';
+$sort = 'ASC';
+$sort_array = array('ACS', 'DESC'); //We will depend on it in get request
+$order_array = array('Ordering', 'ID'); //We will depend on it in get request
 if(isset($_SESSION['username'])){  
 	include 'init.php';
 		//Page Content
@@ -12,11 +16,88 @@ if(isset($_SESSION['username'])){
 $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage'; 
 //Assign do;
 //Create Our Page Depends on the $_GET
-	if($do == 'Manage'){ //Manage Page ?>
-<div class="container">
-		<h1 class="text-center">Manage Categories</h1>
-	    <a href='categories.php?do=Add' class="btn btn-primary">
-	    <i class="fas fa-cart-plus fa-lg"></i> New Category</a>
+	if($do == 'Manage'){ //Manage Page 
+
+		if(isset($_GET['sort']) && in_array($_GET['sort'], $sort_array)){
+			$sort = $_GET['sort'];
+		}	
+		if(isset($_GET['order']) && in_array($_GET['order'], $order_array)){
+			$orderBy = $_GET['order'];
+		}
+
+			$stmt = $connect->prepare("SELECT * FROM categories
+			                           ORDER BY $orderBy $sort");
+			$stmt->execute();
+			$cats = $stmt->fetchAll(); //now we get all the info about the categories
+		?>
+	
+<div class="categories">
+	<div class="container">
+			<h1 class="text-center">Manage Categories</h1>
+		    <a href='categories.php?do=Add' class="btn btn-primary">
+		    <i class="fas fa-cart-plus fa-lg"></i> New Category</a>
+
+		    <div class="panel panel-default">
+		    	<div class="panel-heading">
+		    	Manage BuyIT Sections
+		    	<div class="manage-order pull-right">
+		    		<a href="?sort=ASC" class="<?php
+		    		 if($_GET['sort'] == 'ASC'): echo 'active'; endif;?>">ASC</a> |
+		    		<a href="?sort=DESC" class="<?php
+		    		 if($_GET['sort'] == 'DESC'): echo 'active'; endif;?>">DESC</a>
+		        </div>
+		        <div class="manage-order ordering pull-right">
+		    		<a href="?order=ID" class="<?php
+		    		 if($_GET['order'] == 'ID'): echo 'active'; endif;?>">Date</a> |
+		    		<a href="?order=Ordering" class="<?php
+		    		 if($_GET['order'] == 'Ordering'): echo 'active'; endif;?>">Order-Num</a>
+		        </div>
+		        
+		    </div>
+		    	<div class="panel-body">
+		    		<?php 
+		    			foreach ($cats as $cat) {
+		    			echo "<div class='cat'>";
+		    				echo "<h3> " .  $cat['Name'] . "</h3>";
+		    				echo  "<p>";
+		    				 if(empty($cat['DesBox'])){
+		    				 	echo "This Category has no Description.";
+		    				 } else{
+		    				 	echo $cat['DesBox'];
+		    				 }
+		    				 echo "</p>";
+		    				 //Will Change it
+		    				 if($cat['Visible'] == 0){
+		    				 	echo "<span class='cat-hidden' title='Invisible'>" . "<i class=\"far fa-eye-slash\"></i>" . 
+		    				 	"</span>";
+		    				 }
+		    				 if($cat['Allow_Comment'] == 0){
+		    				 	echo "<span class='cat-comments' title='Comments Off'>" . "<i class=\"fas fa-comment-slash\"></i>" . 
+		    				 	"</span>"; 
+		    				 } 
+		    				 if($cat['Allow_Ads'] == 0){
+		    				 	echo "<span class='cat-ads' title='Adds Off'>" . "
+		    				 	<span class=\"fa-stack fa-1x\">
+                            <i class=\"fab fa-cloudversify fa-stack-1x\"></i>
+                   <i class=\"fas fa-ban fa-stack-2x\" style=\"color:Tomato\"></i>
+</span>" . 
+		    				 	"</span>"; 
+		    				 }
+		    				  //Edit and Delete Buttons (Don't forget to change icons)
+		    				 echo "<div class='hidden-buttons'>";
+		    				 echo "<a href='?do=Edit&catid= ". $cat['ID']  . 
+		    				 "' class='btn  btn-success'><i class=\"fas fa-pen-nib\"></i> Edit</a> ";
+		    				 echo " <a href='#' class='btn  btn-danger confirm'><i class=\"fas fa-trash\"></i> Delete</a>";
+		    				 echo "</div>"; 
+		    			     echo "</div>";
+		    				 echo "<hr>";
+		    				
+		    			}
+		    		?>
+		    	</div>
+		    </div>
+		</div>
+	</div>
 	<?php
 	} elseif($do == 'Add'){ //Add New Category Page?>
 
@@ -24,6 +105,7 @@ $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage';
 	<div class="container">
 		<form class="form-horizontal form-lg" action="?do=Insert" method="POST">
 			<div class="form-group">
+
 				<!--Start Category name-->
 				<div class="form-group">
 					<label class="col-sm-2 control-label">
@@ -118,7 +200,7 @@ $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage';
 						<input type="submit" value="Add Gategory" class="btn btn-primary btn-lg">
 					</div>
 				</div>
-				<!--End FullName-->
+				<!--End Submit-->
 				
 			</div>
 		</form>
@@ -148,7 +230,7 @@ $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage';
 		             redirectHome($error, 'back');
 	            } else{
 	             	//Insert this info into Database 
-				   	$stmt =  $connect->prepare("INSERT INTO `categories`(Name, `desc-box`, Ordering, Visible, Allow_Comment, Allow_Ads) 
+				   	$stmt =  $connect->prepare("INSERT INTO `categories`(Name, DesBox, Ordering, Visible, Allow_Comment, Allow_Ads) 
 				   		VALUES (:name, :des, :order, :vis, :comm , :ads)");
 				   	$stmt->execute(array(
 				   		':name'  => $name,
@@ -177,8 +259,135 @@ $do = isset($_GET['do'])? $_GET['do']: $do = 'Manage';
 		redirectHome($error, 3);
 		
 	}
-	} elseif ($do == 'Edit'){
-		# code...
+	} elseif ($do == 'Edit'){ //Edit Page
+	//Edit from the id with short if condition
+	$catid = isset($_GET['catid']) 
+		&& is_numeric($_GET['catid'])? 
+		intval($_GET['catid']): 0; //That is our user that we would deal with
+		//Now We get the Record from database
+		$stmt = $connect->prepare("SELECT * from categories
+			WHERE ID = ? 
+			LIMIT 1"); //get this user
+		$stmt->execute(array($catid));
+		$row = $stmt->fetch();
+		$count = $stmt-> rowCount();
+		//if We have a record then it must be > 0
+		if($count > 0 ){ ?>
+			<h1 class="text-center">Edit Category</h1>
+	<div class="container">
+		<form class="form-horizontal form-lg" action="?do=Update" method="POST">
+			<div class="form-group">
+				<input type="hidden" name="id" value="<?php echo $catid; ?>">
+				<div class="form-group">
+
+				<!--Start Category name-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Category Name
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="text" name="name" class="form-control"  required="required"
+						 placeholder="The Name of this Gategory"
+						 value="<?php echo $row['Name']; ?>">
+					</div>
+				</div>
+				<!--End Category Name-->
+				<!--Start Description-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Description
+				</label>
+				<div class="col-sm-10 col-md-6">
+				<input type="text" name="desc" class="form-control" placeholder="Describe This Gategory"
+				value="<?php echo $row['DesBox']; ?>">
+				</div>
+				</div>
+				<!--End Description-->
+				<!--Start Ordering-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Order
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="number" name="order" class="form-control"  
+						 placeholder="Enter the Order You Want to show this Category off"
+						 value="<?php echo $row['Ordering']; ?>">
+					</div>
+				</div>
+				<!--End Ordering-->
+				<!--Start Visibility-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Visibility
+				</label>
+					<div class="col-sm-10 col-md-6">
+					<div>
+						<input type="radio" id="vis-yes" name="visibility" value="1" <?php if($row['Visible'] == 1): echo 'checked'; endif;?>>
+						<label for="vis-yes">Yes</label>
+					</div>
+					<div>
+						<input type="radio"  id="vis-no" name="visibility" value="0" <?php if($row['Visible'] == 0): echo 'checked'; endif;?>>
+						<label for="vis-no">No</label>
+					</div>
+					
+				</div>
+			</div>
+				<!--End Visibility-->
+				<!--Start Allow Comments-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Allow Comments
+				 </label>
+					<div class="col-sm-10 col-md-6">
+					<div>
+						<input type="radio" id="com-yes" name="comment" value="1" <?php if($row['Allow_Comment'] == 1): echo 'checked'; endif;?>>
+						<label for="com-yes">Yes</label>
+					</div>
+					<div>
+						<input type="radio"  id="com-no" name="comment" value="0" <?php if($row['Allow_Comment'] == 0): echo 'checked'; endif;?>>
+						<label for="com-no">No</label>
+					</div>
+					
+				</div>
+			</div>
+				<!--End Allow Comments-->
+				<!--Start Allow Ads-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label">
+					Allow Ads
+				</label>
+					<div class="col-sm-10 col-md-6">
+					<div>
+						<input type="radio" id="ads-yes" name="ads" value="1" <?php if($row['Allow_Ads'] == 1): echo 'checked'; endif;?>>
+						<label for="ads-yes">Yes</label>
+					</div>
+					<div>
+						<input type="radio"  id="ads-no" name="ads" value="0" <?php if($row['Allow_Ads'] == 0): echo 'checked'; endif;?>>
+						<label for="ads-no">No</label>
+					</div>
+					
+				</div>
+			</div>
+				<!--End Allow Ads-->
+				<!--Start Submit-->
+				<div class="form-group">
+					<label class="col-sm-2 control-label"> 
+				</label>
+					<div class="col-sm-10 col-md-6">
+						<input type="submit" value="Add Gategory" class="btn btn-primary btn-lg">
+					</div>
+				</div>
+				<!--End Submit-->
+				
+			</div>
+		</form>
+	</div> <?php
+		} else{
+			$errorMsg =  "Wrong There is no such user";
+			redirectHome($errorMsg);
+		}
+	
+       
 	} elseif ($do == 'Update'){
 		# code...
 	} elseif($do == 'Delete'){
